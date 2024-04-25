ae7d05175ccdf38ea37b080858f2bcc385ca6c6a
Rodrigo Burgos <rodrigoburgos@live.com>
2024-04-25 20:24:10 -0300

    feat: ehtereum client

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
+)
+
+func getRpcUrl() string {
+	rpcUrl := os.Getenv("RPC_URL")
+	fmt.Println("RPC_URL: v%", rpcUrl)
+	return rpcUrl
+}
 
 func main() {
-	fmt.Println("Hello Word!")
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := ethclient.Dial(getRpcUrl())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(client.BlockNumber())
 }
