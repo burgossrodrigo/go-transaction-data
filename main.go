a83e7eb5768503bfcd4420a1705cd282da384244
Rodrigo Burgos <rodrigoburgos@live.com>
2024-04-25 20:57:00 -0300

    feat: lastBlockNumber

--- main.go
+++ main.go
@@ -1,30 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/joho/godotenv"
 )
 
-func getRpcUrl() string {
-	rpcUrl := os.Getenv("RPC_URL")
-	fmt.Println("RPC_URL: v%", rpcUrl)
-	return rpcUrl
-}
+/**
+ * Get RPC URL from environment variable
+ */
 
-func main() {
+func getRpcUrl() string {
 
 	if err := godotenv.Load(); err != nil {
 		log.Fatal(err)
 	}
 
+	rpcUrl := os.Getenv("RPC_URL")
+	fmt.Println("RPC_URL: v%", rpcUrl)
+	return rpcUrl
+}
+
+func getBlockchainClient() *ethclient.Client {
 	client, err := ethclient.Dial(getRpcUrl())
 	if err != nil {
 		log.Fatal(err)
 	}
+	return client
+}
 
-	fmt.Println(client.BlockNumber())
+func getLastBlock() *big.Int {
+	client := getBlockchainClient()
+	blockNumber, err := client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return blockNumber.Number
+}
+
+func main() {
+
+	fmt.Println(getLastBlock())
 }
